dde58a9ffadaf8b2ca98d157a350276683ce46cb
Valts Vilcans <valts03@inbox.lv>
2020-07-31 17:41:14 +0200

    [added context / css]

--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+import Transaction from "./Transaction";
 
 const TransactionList = () => {
+  const { transactions } = useContext(GlobalContext);
+
   return (
     <div>
       <h3>History</h3>
       <ul className="list">
-        <li className="minus">
-          Cash <span>-€400</span>
-          <button className="delete-btn">x</button>
-        </li>
+        {transactions.map((el) => (
+          <Transaction key={el.id} transaction={el} />
+        ))}
       </ul>
     </div>
   );
---
+++ src/context/GlobalState.js
@@ -0,0 +1,34 @@
+import React, { createContext, useReducer } from "react";
+import AppReducer from "./AppReducer";
+
+const initialState = {
+  transactions: [
+    { id: 1, text: "Flower", amount: -20 },
+    { id: 2, text: "Salary", amount: 200 },
+    { id: 3, text: "Food", amount: -40 },
+    { id: 4, text: "Insurance", amount: -10 },
+  ],
+};
+
+export const GlobalContext = createContext(initialState);
+
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  function deleteTransaction(id) {
+    dispatch({
+      type: "DELETE_TRANSACTION",
+      payload: id,
+    });
+  }
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        transactions: state.transactions,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
